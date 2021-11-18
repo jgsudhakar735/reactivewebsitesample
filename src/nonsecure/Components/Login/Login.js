784da5706f32b207a581fd9fe1822831497ac764
Sudhakar Tangellapalli <sudhakar.tangellapalli@tagitmobile.com>
2021-11-18 20:53:38 +0530

    ROuter added

--- src/nonsecure/Components/Login/Login.js
+++ src/nonsecure/Components/Login/Login.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect, useNavigate } from 'react-router-dom';
 import './Login.css'
 
 function Login() {
+
+  const [name, setName] = useState({ username: '', password: '' })
+  const [redirect, setRedirect] = useState(false)
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    let uname = data.get("username");
+    let pwd = data.get("password");
+
+    console.log(data)
+    // redirecting to dash board
+    if (uname == 'sudha' && pwd == 'sudha') {
+      // navigate to the next window
+      navigate("/dashboard/users")
+    }
+  }
+
   return (
     <div className="loginGrid">
       <div id="loginBox1">Sample Text Goes Here</div>
       <div id="loginBox">
-        <form className="login" action="" method="post">
+        <form className="login" onSubmit={handleSubmit} method="post">
 
           <div id="headerBox"> Login Here</div>
           <div>
             <label>User Name </label>
-            <input type="text" name="username" id="username" />
+            <input type="text" name="username" id="username" onChange={e => setName({ ...name, username: e.target.value })} />
           </div>
           <div>
             <label>Password </label>
-            <input type="password" name="password" id="password" /></div>
+            <input type="password" name="password" id="password" onChange={e => setName({ ...name, password: e.target.value })} /></div>
 
           <div className="actions" id="submitBox">
             <input type="submit" name="login" value="Login" /> <br />
